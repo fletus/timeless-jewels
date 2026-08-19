13a5f03586dee68d92b06912ac57f5c5be739ab4
fletus <84162778+fletus@users.noreply.github.com>
2026-08-19 19:52:41 +0900

    Pass max total weight to searches

--- frontend/src/lib/worker.ts
+++ frontend/src/lib/worker.ts
@@ -6,7 +6,18 @@ import type { WorkerType } from './sync_worker';
 function getWorker() {
   console.log('Creating sync worker');
   const theWorker = new SyncWorker();
-  const obj = Comlink.wrap<WorkerType>(theWorker);
+  const remote = Comlink.wrap<WorkerType>(theWorker);
+  const obj = {
+    boot: remote.boot,
+    search: (args: Parameters<WorkerType['search']>[0], callback: Parameters<WorkerType['search']>[1]) =>
+      remote.search(
+        {
+          ...args,
+          maxTotalWeight: Number(localStorage.getItem('maxTotalWeight') || 0)
+        },
+        callback
+      )
+  } as typeof remote;
   return { syncWorker: theWorker, syncWrap: obj };
 }
 
